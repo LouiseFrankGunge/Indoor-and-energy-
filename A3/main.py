cc75add2e734cca0adb1b6195a2e78aadb6fa5fc
Olivia2R <s223897@dtu.dk>
2025-10-27 13:49:21 +0100

    Update main.py

--- A3/main.py
+++ A3/main.py
@@ -1,3 +1,43 @@
 import ifcopenshell
 import math
 
+# Load IFC
+model = ifcopenshell.open("IFC models/Group06_2025/25-06-D-MEP.ifc")
+
+
+# Categorized ducts
+round_ducts = []
+rectangular_ducts = []
+
+
+# Loop over all duct segments
+for duct in model.by_type("IfcFlowSegment"):
+    
+    # Check for geometry representation
+    if not duct.Representation:
+        continue
+    
+    # Loop over representations of the duct
+    for representation in duct.Representation.Representations:
+        
+        # Loop over representation items
+        for geometry_item in representation.Items:
+            
+            # Extruded solids
+            if not geometry_item.is_a("IfcExtrudedAreaSolid"):
+                continue
+            
+            # 2D profile
+            profile = geometry_item.SweptArea
+                   
+            # Round ducts
+            if profile.is_a("IfcCircleProfileDef"):
+                diameter = 2 * profile.Radius
+                round_ducts.append((duct, diameter))
+            # Rectangular ducts
+            elif profile.is_a("IfcRectangleProfileDef"):
+                width = profile.XDim
+                height = profile.YDim
+                rectangular_ducts.append((duct, width, height))
+
+print(round_ducts,rectangular_ducts)
